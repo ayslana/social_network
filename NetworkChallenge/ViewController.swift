2f086fe848239aff3a93d744a0bdd4c5eb67535d
ayslana <ayslanariene@gmail.com>
2022-08-15 21:38:06 -0300

    Initial Commit

---
+++ NetworkChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkChallenge
+//
+//  Created by Ayslana Riene on 15/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
